3b2ab711b4cb6e560843023f5f27e9f499e4c697
Nils Witt <nils@nils-witt.de>
2020-11-28 16:53:41 +0100

    removed old functions
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- assets/js/utils.ts
+++ assets/js/utils.ts
@@ -9,19 +9,7 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded");
-    load();
-});
 
-
-
-function load() {
-    loadActive();
-    loadPresets();
-}
-*/
 function textAreaAdjust(textarea) {
     textarea.style.height = "1px";
     textarea.style.height = (3 + textarea.scrollHeight) + "px";
